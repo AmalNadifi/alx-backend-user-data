508d09f8d7a7960446897d268755fc8b46377f43
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-18 18:27:19 +0000

    Task5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,8 +12,44 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+# Load the appropriate authentication instance
+# based on the environment variable AUTH_TYPE
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type:
+    if auth_type == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    elif auth_type == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        raise ValueError(
+                "Invalid value. Supported values: 'auth', 'basic_auth'"
+                )
 
 
+@app.before_request
+def before_request():
+    """Handler for before request filtering"""
+    if auth is None:
+        return  # Do nothing if auth is not set
+
+    excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if (request.path not in excluded_paths and
+            auth.require_auth(request.path, excluded_paths)):
+        # Check if authentication is required for the request path
+        if auth.authorization_header(request) is None:
+            abort(401)  # Authorization header missing, raise 401 error
+        if auth.current_user(request) is None:
+            abort(403)  # Current user not authenticated, raise 403 error
+
+
+# Error handling
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -38,7 +38,9 @@ class Auth:
         Returns:
             str: The authorization header value if present, otherwise None.
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """This method gets the current authenticated user.
