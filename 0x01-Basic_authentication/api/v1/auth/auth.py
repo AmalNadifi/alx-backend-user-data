d4d0715bc4be7243aefde649a0b4f82ab85ad9b2
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-18 17:46:30 +0000

    authentication files created in auth directory

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Module for api authentication"""
+from flask import request
+
+
+class Auth:
+    """Class for managing API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """This method checks if authentication is required for a given path.
+
+        Args:
+            path (str): The path to check for authentication requirement.
+            excluded_paths (List[str]): Paths excluded from authentication
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """This method gets the authorization header from the request.
+
+        Args:
+            request (Request): The Flask request object.
+
+        Returns:
+            str: The authorization header value if present, otherwise None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This method gets the current authenticated user.
+
+        Args:
+            request (Request): The Flask request object.
+
+        Returns:
+            TypeVar('User'): Current authenticated user object if available
+                            otherwise None.
+        """
+        return None
