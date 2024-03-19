a943f3a36203171d0a7f7fb3f9d643d3e338477b
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-19 16:54:25 +0000

    pep8 fixed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    """ This is the class of BasicAuth"""
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """
