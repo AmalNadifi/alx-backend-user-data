d6f93c822d01a7816b95f2a4c57204a04925da92
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-26 10:06:57 -0700

    Task12. Find user by session ID-update3.0

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str) -> Union[user, None]:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """
         This function retrievs a user from a session_id
         Args:
