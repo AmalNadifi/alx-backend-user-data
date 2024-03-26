13a13339f452bfd68f21ef404061e1187223ff72
AmalNadifi <amal.nadifi9@gmail.com>
2024-03-26 08:01:33 -0700

    Task2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -45,3 +47,27 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        This function retrieves a user whose attributes match
+        the attributes provided as arguments.
+
+        Args:
+            kwargs (dict): A dictionary of attributes to match the user.
+
+        Returns:
+            User: The matching user if found.
+
+        Raises:
+            InvalidRequestError: If an invalid attribute is provided.
+            NoResultFound: If no user is found with the provided attributes.
+        """
+        all_users = self._session.query(User)
+        for attr_name, attr_value in kwargs.items():
+            if attr_name not in User.__dict__:
+                raise InvalidRequestError
+            for user in all_users:
+                if getattr(user, attr_name) == attr_value:
+                    return user
+        raise NoResultFound 
